a644da622acb2b26106990f976e64c5811e46d9c
kashomon <jrhoak@gmail.com>
2015-06-01 23:42:20 -0600

    More work on system tests.

--- glicko/system_test.go
+++ glicko/system_test.go
@@ -1,9 +1,21 @@
 package goglicko
 
 import (
+	"github.com/Kashomon/gorating"
 	"testing"
 )
 
+type tgame struct {
+}
+
+func (t *tgame) PlayerOne() gorating.PlayerRating { return nil }
+func (t *tgame) PlayerTwo() gorating.PlayerRating { return nil }
+func (t *tgame) GameResult() float64              { return 0 }
+
 func TestFoo(t *testing.T) {
-	t.Errorf("Couldn't pass")
+	_ = &System{}
+	_ = NewRating(1500, 200, DefaultVol)
+	_ = []gorating.Game{
+		&tgame{},
+	}
 }
